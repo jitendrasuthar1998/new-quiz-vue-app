1986db82562fe35a91386180122d5e17891b1d32
jitendradev <jitendra.suthar@growcify.com>
2021-08-12 17:36:38 +0530

    created a new-quiz app in vue

---
+++ README.md
@@ -0,0 +1,18 @@
+# new-quiz
+This is a quiz app created by using Vue.js.
+
+The questions in this application are comming from an api.
+
+To style this application's view, i have used BootStrapVue.
+
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="question-box-container">
+      <div>
+        <b-jumbotron>
+       
+          <template #lead>
+            Same question 
+          </template>
+
+          <hr class="my-4" />
+
+          <p>
+            List Of Answers 
+          </p>
+
+          <b-button variant="primary" href="#">Submit</b-button>
+          <b-button variant="success" href="#">Next</b-button>
+        </b-jumbotron>
+      </div>
+
+  </div>
+</template>

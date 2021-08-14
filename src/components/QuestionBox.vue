f893bc06c43d408202a67fd4cf38710cbdaada5c
jitendradev <jitendra.suthar@growcify.com>
2021-08-14 17:47:40 +0530

    installed loadash library to shuffle list of answer-options and used in QuestionBox component

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -9,17 +9,14 @@
         <hr class="my-4" />
 
         <b-list-group>
-          <b-list-group-item v-for="(answer, index) in answers" :key="index"
-          @click.prevent="selectAnswer(index)"
-          :class="[selectedIndex === index ? 'selected' : null]"
-          >{{
-            answer
-          }}</b-list-group-item>
+          <b-list-group-item
+            v-for="(answer, index) in answers"
+            :key="index"
+            @click.prevent="selectAnswer(index)"
+            :class="[selectedIndex === index ? 'selected' : null]"
+            >{{ answer }}</b-list-group-item
+          >
         </b-list-group>
-
-        <!-- <p v-for="(answer,index) in answers" :key="index">
-            {{answer}}
-          </p> -->
 
         <b-button variant="primary" href="#">Submit</b-button>
         <b-button @click="next" variant="success" href="#">Next</b-button>
@@ -29,15 +26,17 @@
 </template>
 
 <script>
+import _ from 'lodash';
 export default {
   props: {
     currentQuestion: Object,
     next: Function,
   },
-  data(){
-return{
-  selectedIndex: null
-}
+  data() {
+    return {
+      selectedIndex: null,
+      shuffledAnswers: [],
+    };
   },
   computed: {
     answers() {
@@ -48,48 +47,54 @@ return{
       return answers;
     },
   },
-  methods:{
-selectAnswer(index){
-  this.selectedIndex = index;
-  console.log('selectedIndex of answer of is', this.selectedIndex);
- // console.log('index of this answer is', index)
-}
+  watch: {
+    currentQuestion() {
+      this.selectedIndex = null;
+      this.shuffleAnswers();
+    },
   },
-  mounted() {
-    console.log(this.currentQuestion);
-  },
+  methods: {
+    selectAnswer(index) {
+      this.selectedIndex = index;
+      console.log("selectedIndex of answer of is", this.selectedIndex);
+      // console.log('index of this answer is', index)
+    },
+    shuffleAnswers() {
+      let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer];
+      this.shuffledAnswers = _.shuffle(answers)
+    }
+  }
 };
 </script>
 
 <style scoped>
-
-.lead{
+.lead {
   margin: 10px;
   margin-top: 30px;
   font-size: 30px;
 }
-.list-group{
+.list-group {
   margin-bottom: 20px;
   font-size: 20px;
 }
 
-.list-group-item:hover{
- background: #EEE;
-cursor: pointer;
+.list-group-item:hover {
+  background: #eee;
+  cursor: pointer;
 }
-.btn{
+.btn {
   margin: 10px;
 }
 
-.selected{
+.selected {
   background-color: lightblue;
 }
 
-.correct{
+.correct {
   background-color: lightgreen;
 }
 
-.incorrect{
+.incorrect {
   background-color: red;
 }
 </style>
